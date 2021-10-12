4fde7bd5cc57b02859ead55eb26d817f004bf124
Liam Paternott <38994791+lpat0002@users.noreply.github.com>
2021-10-13 08:13:34 +1100

    initial

---
+++ W10_vglite.js
@@ -0,0 +1,22 @@
+var vg_1 = "W10HW_Bar.vega";
+var vg_2 = "W10HW_Map.vega";
+var vg_3 = "A2_Scatter2.vega";
+
+vegaEmbed("#barchart", vg_1).then(function(result) {
+
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
+
+vegaEmbed("#chmap", vg_2).then(function(result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   
+   }).catch(console.error);
+
+vegaEmbed("#scatterplot", vg_3).then(function(result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   
+   }).catch(console.error);
+   
